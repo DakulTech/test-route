2d37a50f8f725714dc839c461e643dcbf9d8b086
Mutiullahi A Uthman <113379487+DakulTech@users.noreply.github.com>
2025-11-11 21:07:19 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
+app.use((req, res, next) => {
+  console.log(`🔍 Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
 // 404 fallback
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
